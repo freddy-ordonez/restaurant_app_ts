7af66c222ddb342bc519774c70a013ed47acbc56
Freddy Antonio Ordonez Aguilar <freddy12aguilar@outlook.com>
2024-05-12 20:02:34 -0600

    se agrego esquemas de validacion para user y las rutas

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { validacionSchemas } from "../middleware/middleware";
 import { addUser, deleteUser, getOneUser, getUsers, updateUser } from "../controller/user.controller";
+import { addUserSchema, idUserSchema, updateUserSchema } from "../validation/userSchema";
 
 const router = Router()
 
-router.get('/users', validacionSchemas, getUsers)
-router.get('/users/:id', validacionSchemas, getOneUser)
+router.get('/users', getUsers)
+router.get('/users/:id', idUserSchema, validacionSchemas, getOneUser)
 
-router.post('/users', validacionSchemas, addUser)
+router.post('/users', addUserSchema, validacionSchemas, addUser)
 
-router.put('/users/:id', validacionSchemas, updateUser)
+router.put('/users/:id', updateUserSchema, validacionSchemas, updateUser)
 
-router.delete('/users/:id', validacionSchemas, deleteUser)
+router.delete('/users/:id',idUserSchema, validacionSchemas, deleteUser)
 
 export default router;
---
+++ api/src/validation/userSchema.ts
@@ -0,0 +1,118 @@
+import { checkSchema } from "express-validator";
+
+export const addUserSchema = checkSchema({
+    email: {
+        in:['body'],
+        exists: {
+            errorMessage: "email is required"
+        },
+        isLength: {
+            options: {
+                max:100
+            },
+            errorMessage: "email max 100 characters"
+        },
+        isString: {
+            errorMessage: 'email must be in string'
+        },
+        notEmpty: {
+            errorMessage: 'email cant be empty'
+        },
+        isEmail: {
+            errorMessage: 'format email is invalid'
+        }
+    },
+    password: {
+        in:['body'],
+        exists: {
+            errorMessage: "password is required"
+        },
+        isLength: {
+            options: {
+                max:16
+            },
+            errorMessage: "password max 16 characters"
+        },
+        isString: {
+            errorMessage: 'password must be in string'
+        },
+        notEmpty: {
+            errorMessage: 'password cant be empty'
+        },
+        isStrongPassword: {
+            options: {
+                minLength: 8,
+                minLowercase: 1,
+                minUppercase: 1,
+                minSymbols: 1
+            },
+            errorMessage: "password must contain min 8 character and symbols, letters in uppercase and lowercase example 'Ironman@'"
+        }
+    }
+})
+
+export const updateUserSchema = checkSchema({
+    id: {
+        in: ['params'],
+        isNumeric: {
+            errorMessage: "id must be number"
+        }
+    },
+    email: {
+        in:['body'],
+        exists: {
+            errorMessage: "email is required"
+        },
+        isLength: {
+            options: {
+                max:100
+            },
+            errorMessage: "email max 100 characters"
+        },
+        isString: {
+            errorMessage: 'email must be in string'
+        },
+        notEmpty: {
+            errorMessage: 'email cant be empty'
+        },
+        isEmail: {
+            errorMessage: 'format email is invalid'
+        }
+    },
+    password: {
+        in:['body'],
+        exists: {
+            errorMessage: "password is required"
+        },
+        isLength: {
+            options: {
+                max:16
+            },
+            errorMessage: "password max 16 characters"
+        },
+        isString: {
+            errorMessage: 'password must be in string'
+        },
+        notEmpty: {
+            errorMessage: 'password cant be empty'
+        },
+        isStrongPassword: {
+            options: {
+                minLength: 8,
+                minLowercase: 1,
+                minUppercase: 1,
+                minSymbols: 1
+            },
+            errorMessage: "password must contain min 8 character and symbols, letters in uppercase and lowercase example 'Ironman@'"
+        }
+    }
+})
+
+export const idUserSchema = checkSchema({
+    id: {
+        in: ['params'],
+        isNumeric: {
+            errorMessage: "id must be number"
+        }
+    }
+})
